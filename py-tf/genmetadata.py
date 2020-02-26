7aad9cc444c0d027da28f37515cd23d5859d9d4a
Jim <38408451+jfurches@users.noreply.github.com>
2020-02-26 18:20:48 -0500

    Upgraded metadata generator with args #8

--- py-tf/genmetadata.py
+++ py-tf/genmetadata.py
@@ -2,12 +2,31 @@ import json
 import numpy as np
 import cv2
 from glob import glob
+import argparse
+from pathlib import Path
+from multiprocessing import Pool
 
-# Take the file name and remove *-mask.jpg part
+def get_args(args=None):
+    """
+    Parses the command line args according to
+    $ python genmetadata.py [dir]
+    """
+    parser = argparse.ArgumentParser(description='Creates metadata for samples for later use in create_sample_images.py')
+    parser.add_argument('dir', type=str, default='./samples',
+                        help="Input directory of initial samples to process")
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help="Verbose output")
+    return parser.parse_args() if args is None else parser.parse_args(args)
+
+# Take the file name and remove *-mask part
 def nameToKey(name):
-    return name.split("-")[0].split("\\")[1]
+    return name.split("-")[0]
 
-def process_image(name):
+def process_image(path):
+    """
+    Computes average target location of an image (or returns [-1, -1] if there is no target in the mask)
+    """
+    name = str(path)
     img = cv2.imread(name) # read image
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # make black & white
     
@@ -17,20 +36,42 @@ def process_image(name):
         # Compute average target location
         indices = img.nonzero()
         avg = [np.mean(indices[1]), np.mean(indices[0])]
-        return (True, avg)
+        return avg
     else:
-        return (False, None)
+        return [-1, -1]
 
-json_file = "./metadata.json"
-files = glob("./samples/masks/*.jpg")
+if __name__ == "__main__":
+    args = get_args()
+    samples_dir = Path(args.dir)
 
-names = list(map(nameToKey, files))
-out = list(map(process_image, files))
+    # Obviously we can't do anything if the path doesn't exist
+    assert samples_dir.exists()
 
-result = {}
-for i in range(len(files)):
-    if out[i][0]:
-        result[names[i]] = out[i][1]
+    # Metadata output file
+    json_file = samples_dir / 'metadata.json'
 
-with open(json_file, "w") as f:
-    json.dump(result, f)
+    # Detected sample files
+    files = list((samples_dir / 'masks').glob("*.jpg"))
+
+    if args.verbose:
+        print("Detected files: ")
+        for f in files:
+            print(f'- {f}')
+
+    # Directory is empty and has no samples
+    assert len(files) > 0
+
+    names = [nameToKey(x.stem) for x in files]
+
+    # Use multiprocessing to speed this stuff up
+    p = Pool(4)
+    out = p.map(process_image, files)
+
+    # Create metadata
+    result = {}
+    for pair in zip(names, out):
+        result[pair[0]] = pair[1]
+
+    # Write metadata
+    with open(json_file, "w") as f:
+        json.dump(result, f)
